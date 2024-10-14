a43ae973c8afcd7df6bb03ba136cbde88433fce7
AFSB114 <asuazab13@gmail.com>
2024-10-14 18:51:22 -0500

    Add Allow Methods

--- php/controller/log.php
+++ php/controller/log.php
@@ -4,8 +4,9 @@ include_once '../connection/connection.php';
 include_once '../schema/user.php';
 include_once '../schema/log.php';
 
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: *");
 
 $req = json_decode(file_get_contents("php://input"), true);
 
